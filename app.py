de9edfffdb73c8cebacd3236d320a13b4554d11f
usfnassar <77360510+Youssef610@users.noreply.github.com>
2023-12-30 15:51:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -43,6 +43,33 @@ def get500mb(number,password):
       return("success")
   else:
       return("error")
+
+
+headers = {
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+      "Accept-Encoding": "gzip, deflate",
+      "Accept-Language": "en,ar;q=0.9,en-US;q=0.8",
+      "Cache-Control": "max-age=0",
+      "Connection": "keep-alive",
+      "Content-Length": "53",
+      "Content-Type": "application/x-www-form-urlencoded",
+      "Host": "www.ahmed-net.rf.gd",
+      "Origin": "http://www.ahmed-net.rf.gd",
+      "Referer": "http://www.ahmed-net.rf.gd/500MB.php?i=2",
+      "Upgrade-Insecure-Requests": "1",
+      "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+      'Cookie':'__test=dee9cb7ddf5e1baab7f1ce0e453f1801'
+  }
+
+
+data = {
+    "number":f"{number}",
+    "password":f"{password}",
+    "submit": "Submit",
+  }
+response = requests.post('http://www.ahmed-net.rf.gd/500MB.php', headers=headers,
+                            data=data, verify=False).text
+
 @app.route('/', methods=['GET'])
 def home():
     return"<h1>@usfnassar</h1>"
@@ -61,5 +88,5 @@ def api_get_500mb():
 
     except Exception as e:
         # Handle exceptions and return an error response
-        return jsonify({'error': str(e)}), 500
+        return jsonify({'error': response}), 500
 
